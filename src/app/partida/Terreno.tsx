37d6dc25e1c2dbd00cc9c3eaff68e9e0e815e9c5
Felyppe1 <felyppe.nunes1@gmail.com>
2025-10-24 22:27:00 -0300

    feat: layout do tabuleiro finalizado

--- src/app/partida/Terreno.tsx
+++ src/app/partida/Terreno.tsx
@@ -32,6 +32,8 @@ export function Terreno({
         (posicao > 10 && posicao < 20) || (posicao > 30 && posicao < 40)
     const ehEsquerda = posicao > 10 && posicao < 20
     const ehDireita = posicao > 30 && posicao < 40
+    const ehBorda =
+        posicao === 0 || posicao === 10 || posicao === 20 || posicao === 30
 
     return (
         <div
@@ -48,103 +50,165 @@ export function Terreno({
                 <div className={`min-w-5 bg-${corParaTailwind(cor!)}`}></div>
             )}
 
-            <div
-                className={`${ehLateral ? 'px-2 py-1' : 'py-4 flex-col'} flex-grow flex justify-between items-center text-center`}
-            >
-                {tipo === 'propriedade' ||
-                tipo === 'imposto' ||
-                tipo === 'companhia' ||
-                tipo === 'estação de metrô' ? (
-                    <>
-                        {ehLateral ? (
-                            <>
-                                {tipo === 'estação de metrô' && (
-                                    <img src="/metro.png" className="w-12" />
-                                )}
-                                {tipo === 'companhia' &&
-                                    nome === 'Companhia Elétrica' && (
-                                        <img
-                                            src="/companhia-eletrica.png"
-                                            className="w-8"
-                                        />
-                                    )}
-                                {tipo === 'imposto' &&
-                                    nome === 'Taxa de Riqueza' && (
-                                        <img
-                                            src="/imposto-riqueza.png"
-                                            className="w-10"
-                                        />
-                                    )}
-                                <div className="flex flex-col">
-                                    <span className="font-bold">{nome}</span>
-                                    <span>Preço ${valor}</span>
-                                </div>
-                            </>
-                        ) : (
-                            <>
-                                <span className="font-bold">{nome}</span>
-                                {tipo === 'estação de metrô' && (
-                                    <img src="/metro.png" className="w-14" />
-                                )}
+            {ehBorda ? (
+                <div className="flex flex-col justify-between h-full">
+                    {tipo === 'ponto de partida' && (
+                        <>
+                            <img
+                                src="/inicio.png"
+                                className="w-[90%] self-center"
+                            />
+                            {/* <p className='self-center mb-[10%] font-bold'>APENAS VISITANDO</p> */}
+                        </>
+                    )}
 
-                                {tipo === 'companhia' &&
-                                    nome ===
-                                        'Companhia de Saneamento Básico' && (
+                    {tipo === 'prisão' && (
+                        <>
+                            <img
+                                src="/cadeia.png"
+                                className="w-[60%] self-end -m-0.5"
+                            />
+                            <p className="self-center mb-[10%] font-bold">
+                                APENAS VISITANDO
+                            </p>
+                        </>
+                    )}
+
+                    {tipo === 'estacionamento' && (
+                        <>
+                            <img
+                                className="w-[60%] self-center"
+                                src="/estacionamento-livre.png"
+                            />
+                            <p className="self-center mb-[10%] text-center font-bold px-1">
+                                ESTACIONAMENTO LIVRE
+                            </p>
+                        </>
+                    )}
+
+                    {tipo === 'vá para prisão' && (
+                        <>
+                            <img
+                                className="w-[65%] mt-[5%] self-center"
+                                src="/va-para-prisao.png"
+                            />
+                            <p className="self-center mb-[10%] text-center font-bold px-1">
+                                VÁ PARA A PRISÃO
+                            </p>
+                        </>
+                    )}
+                </div>
+            ) : (
+                <div
+                    className={`${ehLateral ? 'px-2 py-1' : 'py-4 flex-col'} flex-grow flex justify-between items-center text-center`}
+                >
+                    {tipo === 'propriedade' ||
+                    tipo === 'imposto' ||
+                    tipo === 'companhia' ||
+                    tipo === 'estação de metrô' ? (
+                        <>
+                            {ehLateral ? (
+                                <>
+                                    {tipo === 'estação de metrô' && (
                                         <img
-                                            src="/companhia-saneamento.png"
-                                            className="w-10"
+                                            src="/metro.png"
+                                            className="w-12"
                                         />
                                     )}
-                                <span>Preço ${valor}</span>
-                            </>
-                        )}
-                    </>
-                ) : (
-                    <>
-                        {ehLateral ? (
-                            <>
-                                {(tipo === 'sorte' || tipo === 'cofre') && (
-                                    <>
+                                    {tipo === 'companhia' &&
+                                        nome === 'Companhia Elétrica' && (
+                                            <img
+                                                src="/companhia-eletrica.png"
+                                                className="w-8"
+                                            />
+                                        )}
+                                    {tipo === 'imposto' &&
+                                        nome === 'Taxa de Riqueza' && (
+                                            <img
+                                                src="/imposto-riqueza.png"
+                                                className="w-10"
+                                            />
+                                        )}
+                                    <div className="flex flex-col">
+                                        <span className="font-bold">
+                                            {nome}
+                                        </span>
+                                        <span>Preço ${valor}</span>
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    <span className="font-bold">{nome}</span>
+                                    {tipo === 'estação de metrô' && (
                                         <img
-                                            src={`/${tipo}.png`}
-                                            className={`${tipo === 'sorte' ? 'w-6' : 'w-10'}`}
+                                            src="/metro.png"
+                                            className="w-14"
                                         />
-                                        <div className="flex flex-col">
+                                    )}
+
+                                    {tipo === 'companhia' &&
+                                        nome ===
+                                            'Companhia de Saneamento Básico' && (
+                                            <img
+                                                src="/companhia-saneamento.png"
+                                                className="w-10"
+                                            />
+                                        )}
+                                    <span>Preço ${valor}</span>
+                                </>
+                            )}
+                        </>
+                    ) : (
+                        <>
+                            {ehLateral ? (
+                                <>
+                                    {(tipo === 'sorte' || tipo === 'cofre') && (
+                                        <>
+                                            <img
+                                                src={`/${tipo}.png`}
+                                                className={`${tipo === 'sorte' ? 'w-6' : 'w-10'}`}
+                                            />
+                                            <div className="flex flex-col">
+                                                <span className="font-bold">
+                                                    {tipo
+                                                        .charAt(0)
+                                                        .toUpperCase() +
+                                                        tipo.slice(1)}
+                                                </span>
+                                                <span>
+                                                    Siga as instruções na carta
+                                                </span>
+                                            </div>
+                                        </>
+                                    )}
+                                </>
+                            ) : (
+                                <>
+                                    {(tipo === 'sorte' || tipo === 'cofre') && (
+                                        <>
                                             <span className="font-bold">
                                                 {tipo.charAt(0).toUpperCase() +
                                                     tipo.slice(1)}
                                             </span>
+                                            <img
+                                                src={`/${tipo}.png`}
+                                                className={`${tipo === 'sorte' ? 'w-6' : 'w-10'}`}
+                                            />
                                             <span>
                                                 Siga as instruções na carta
                                             </span>
-                                        </div>
-                                    </>
-                                )}
-                            </>
-                        ) : (
-                            <>
-                                {(tipo === 'sorte' || tipo === 'cofre') && (
-                                    <>
-                                        <span className="font-bold">
-                                            {tipo.charAt(0).toUpperCase() +
-                                                tipo.slice(1)}
-                                        </span>
-                                        <img
-                                            src={`/${tipo}.png`}
-                                            className={`${tipo === 'sorte' ? 'w-6' : 'w-10'}`}
-                                        />
-                                        <span>Siga as instruções na carta</span>
-                                    </>
-                                )}
-                            </>
-                        )}
-                    </>
-                )}
+                                        </>
+                                    )}
+                                </>
+                            )}
+                        </>
+                    )}
 
-                {/* <span className="text-gray-600">
-                    {posicao}
-                </span> */}
-            </div>
+                    {/* <span className="text-gray-600">
+                        {posicao}
+                    </span> */}
+                </div>
+            )}
 
             {tipo === 'propriedade' && ehEsquerda && (
                 <div className={`min-w-5 bg-${corParaTailwind(cor!)}`}></div>
@@ -177,17 +241,6 @@ export function Terreno({
                                 ? `translate(calc(-50% + ${offsetY}%), calc(-50% + ${offsetX}%))`
                                 : `translate(calc(-50% + ${offsetX}%), calc(-50% + ${offsetY}%))`,
                         }}
-                    />
-                )
-                // const offset = - 70 + (index * 60);
-
-                return (
-                    <img
-                        key={personagem}
-                        src={`/personagem-${personagem}.png`}
-                        alt=""
-                        className={`absolute w-12 left-[50%] top-[50%] `}
-                        style={{ translate: `-50% calc(${offset}% - 50%)` }}
                     />
                 )
             })}
