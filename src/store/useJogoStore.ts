8988e4ae8622c9b8341293b055fdf794ca66c321
Felyppe1 <felyppe.nunes1@gmail.com>
2025-11-12 18:53:57 -0300

    refactor: jogo store

--- src/store/useJogoStore.ts
+++ src/store/useJogoStore.ts
@@ -1,26 +1,15 @@
-import { Jogo, JogoOutput } from '@/domain/jogo'
+import { Jogo } from '@/domain/jogo'
 import { create } from 'zustand'
 
 interface JogoStore {
     jogo: Jogo | null
-    estadoJogo: JogoOutput | null
 
     setJogo: (jogo: Jogo) => void
-    setEstadoJogo: (estado: JogoOutput) => void
     resetarJogo: () => void
-
-    jogarDados: () => {
-        dado1: number
-        dado2: number
-        saiuDaPrisao: boolean
-        jogadorPermaneceuPreso: boolean
-        jogadorFoiParaPrisao: boolean
-    } | null
 }
 
 const estadoInicial = {
     jogo: null,
-    estadoJogo: null,
 }
 
 export const useJogoStore = create<JogoStore>((set, get) => ({
@@ -32,25 +21,10 @@ export const useJogoStore = create<JogoStore>((set, get) => ({
                 Object.create(Object.getPrototypeOf(jogo)),
                 jogo,
             ),
-            estadoJogo: jogo.toObject(),
         }))
-    },
-
-    setEstadoJogo: estado => {
-        set(() => ({ estadoJogo: estado }))
     },
 
     resetarJogo: () => {
         set(() => estadoInicial)
-    },
-
-    jogarDados: () => {
-        const { jogo } = get()
-        if (!jogo) return null
-
-        const resultado = jogo.jogarDados()
-
-        set(() => ({ estadoJogo: jogo.toObject() }))
-        return resultado
     },
 }))
